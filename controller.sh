198858cb9934b7f5a4d594b317eb7e64750ad274
Utilisateur <ayoub.mouak.2015@gmail.com>
2024-04-20 02:26:24 +0200

    init commit

--- controller.sh
+++ controller.sh
@@ -9,6 +9,12 @@ exit
 #Install git
 sudo yum install -y git
 
+git clone https://github.com/MOUAK-Ayoub/controller.git
+cd controller
 # Install python
 ./scripts/python_installation.sh
+# Install ansible
 ./scripts/ansible_installation.sh
+
+# Install jenkins, docker,..etc
+ansible-playbook  playbooks/main.yml
--- tmp.sh
+++ tmp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/MOUAK-Ayoub/controller.git
 ./scripts/ansible_installation.sh
 ansible-playbook  playbooks/main.yml
