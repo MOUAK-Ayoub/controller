962421b7257f3389d156cb131d6473fadf7dee6f
ayoub.hp <ayoub.hp@com>
2024-05-06 15:08:19 +0100

    Add playbook for installing artifactory

--- controller.sh
+++ controller.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-python_version="Python 3.9.6"
-[[ $(python3 --version) == $python_version ]]; is_correct_version=$?
+python_version="3.9"
 
 chmod +x scripts/*
 
-if  (( is_correct_version==1 ))
+# check if the python version contains 3.9
+if [[ "$(python3 --version)" =~ ${python_version} ]]
 then
-    # Install python 3.9.6
     ./scripts/python_installation.sh
 fi
 
