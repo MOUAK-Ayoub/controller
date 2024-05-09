967fd4a05abdb5a0eec65f8ef6afc020a67e98f4
MOUAK-AYOUB <ayoub.mouak.2015@gmail.com>
2024-05-09 18:13:14 +0100

    Add pip install for distro that has just python like rhel9

--- controller.sh
+++ controller.sh
@@ -3,16 +3,17 @@
 python_version="3.9"
 
 chmod +x scripts/*
-
+is_installed=1
 # check if the python version contains 3.9
 if ! [[ "$(python3 --version)" =~ ${python_version} ]]
 then
     echo "Installing python 3.9.6"
     ./scripts/python_installation.sh
+    is_installed=0
 fi
 
 
-if  [[ TO-DO ]]
+if  (( is_installed==1 ))
 then
     echo "Installing pip3 "
     sudo yum install -y python3-pip
